d56b83306891f3a23d3e415066525be93dbcf412
Jackson Barbosa <jbs@ic.ufal.br>
2018-04-05 14:13:38 -0300

    Adicionado o placar e o jogo estatico entre dois jogadores

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-function start() {
+window.onload = function () {
 	jogador_1 = prompt("Digite o nome do primeiro jogador");
 	jogador_2 = prompt("Digite o nome do segundo jogador");
+	document.getElementById("nome_1").innerHTML = "Jogador 1: " + jogador_1;
+	document.getElementById("nome_2").innerHTML = "Jogador 2: " + jogador_2;
+}
+
+function start() {
+	jogador_1 = document.getElementById("nome_1").innerHTML;
+	jogador_2 = document.getElementById("nome_2").innerHTML;
 	num = parseInt((Math.random() * 20) + 1);
 	game(num, jogador_1, jogador_2, 0, 0);
 }
 
 function vitoria(jogador, qtd_jogadas) {
+	chute_errado = (qtd_jogadas-1) > 0 ? (qtd_jogadas-1) : 0;
 	msg = jogador + " Venceu\nResumo do jogo\n" + jogador + ": deu " + (qtd_jogadas-1) + " chutes errados totalizando " + qtd_jogadas + " jogadas";
 	alert(msg);
+}
+
+function derrota(jogador, qtd_jogadas) {
+	chute_errado = (qtd_jogadas-1) > 0 ? (qtd_jogadas-1) : 0;
+	msg = jogador + " Você perdeu, se esforce mais\nResumo do jogo\n" + jogador + ": deu " + (qtd_jogadas-1) + " chutes errados totalizando " + qtd_jogadas + " jogadas";
+	alert(msg)
 }
 
 function game(num, jogador_1, jogador_2, qtd_turnos_jog_1, qtd_turnos_jog_2) {
 	jogada1 = parseInt(prompt("Olá " + jogador_1 + ", qual seu chute?", ""));
 	qtd_turnos_jog_1++;
 	if(num == jogada1) {
+		value = parseInt(document.getElementById("pontos_j1").innerHTML);
+		document.getElementById("pontos_j1").innerHTML = (value+1);
 		vitoria(jogador_1, qtd_turnos_jog_1);
+		derrota(jogador_2, qtd_turnos_jog_2);
 		return;
 	} else if(num > jogada1){
 		alert(jogada1 + " é menor que o numero sorteado");
@@ -25,7 +42,10 @@ function game(num, jogador_1, jogador_2, qtd_turnos_jog_1, qtd_turnos_jog_2) {
 	jogada2 = parseInt(prompt("Olá " + jogador_2 + ", qual seu chute?", ""));
 	qtd_turnos_jog_2++;
 	if(num == jogada2) {
+		value = parseInt(document.getElementById("pontos_j2").innerHTML);
+		document.getElementById("pontos_j2").innerHTML = (value+1);
 		vitoria(jogador_2, qtd_turnos_jog_2);
+		derrota(jogador_1, qtd_turnos_jog_1);
 		return;	
 	} else if(num > jogada2){
 		alert(jogada2 + " é menor que o numero sorteado");
