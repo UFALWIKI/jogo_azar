3355b7a0ea3a79849c1b6fd43c0556983ae8b563
-l <pjpc@ic.ufal.br>
2018-04-06 20:53:51 -0300

    adicionei na leitura do prompt a idade dos jogadores, por requerimento da questao

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 window.onload = function () {
 	jogador_1 = prompt("Digite o nome do primeiro jogador");
+	idade_jogador_1 = prompt("Digite a idade do primeiro jogador");
 	jogador_2 = prompt("Digite o nome do segundo jogador");
-	document.getElementById("nome_1").innerHTML = "Jogador 1: " + jogador_1;
-	document.getElementById("nome_2").innerHTML = "Jogador 2: " + jogador_2;
+	idade_jogador_2 = prompt("Digite a idade do segundo jogador");
+	
+	document.getElementById("nome_1").innerHTML = "Jogador 1: " + jogador_1 + " / idade : " + idade_jogador_1 + "anos";
+	document.getElementById("nome_2").innerHTML = "Jogador 2: " + jogador_2 + " / idade : " + idade_jogador_2 + "anos";
 }
 
 function start() {
